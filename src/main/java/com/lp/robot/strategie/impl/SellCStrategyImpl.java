0b57ee09f8ef1c79660423a15da863720fd3fb1c
HaiBo <sihaibo_sy@163.com>
2022-03-30 17:55:19 +0800

    取消添加判断

--- src/main/java/com/lp/robot/strategie/impl/SellCStrategyImpl.java
+++ src/main/java/com/lp/robot/strategie/impl/SellCStrategyImpl.java
@@ -100,7 +100,12 @@ public class SellCStrategyImpl implements StrategyProvider {
                     if (sellTradeOrder.getCreateDateTime().plusMinutes(3).compareTo(LocalDateTime.now()) > 0) {
                         return;
                     }
-                    gateIoCommon.cancel(tradeOrder.getSymbol(), sellOrderNumber.toPlainString());
+                    // 判断下，数量是否还够，交易所可能挂单卖出去一部分了。
+                    final BigDecimal balances = gateIoCommon.balances(tradeOrder.getSymbol().replace("_usdt", ""));
+                    // 资金足够的时候 才可以取消订单
+                    if (balances.compareTo(order.getTradeNumber()) >= 0) {
+                        gateIoCommon.cancel(tradeOrder.getSymbol(), sellOrderNumber.toPlainString());
+                    }
                 }
                 return;
             }
